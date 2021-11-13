5f2032c46e88b1a2bb429854fc519cf16037b4cd
Shahzeb Khan <shahzeb8285@gmail.com>
2021-11-13 10:07:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# knight-info-build
---
+++ precache-manifest.26c256234e3fd8c46dc6298db096c115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83ac249ea3008f328b86e7050d4b71e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86f8864890c3739907da",
+    "url": "/static/js/2.af878daf.chunk.js"
+  },
+  {
+    "revision": "8efadbe79de939f183de4c88d2491df0",
+    "url": "/static/js/2.af878daf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1454cef04042cfe5a9ab",
+    "url": "/static/js/main.42e62d01.chunk.js"
+  },
+  {
+    "revision": "551c9e280db88d76f240",
+    "url": "/static/js/runtime-main.c307dd36.js"
+  },
+  {
+    "revision": "66ad3242972fe260304197d77cf77225",
+    "url": "/static/media/logo.66ad3242.svg"
+  }
+]);
